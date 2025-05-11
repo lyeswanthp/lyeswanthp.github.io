d1ef068117516cfa66aa54c949ca7c7f647d1928
Lovely Yeswanth Panchumarthi <lpanch2@emory.edu>
2025-05-11 00:21:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,11 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{lyeswanthp.github.io}' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/{lyeswanthp.github.io}' : '',
+  // For a project page (username.github.io/project-name):
+  // If your repository is named "lyeswanthp.github.io", and it's a user site,
+  // you should use empty strings for both:
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
 module.exports = nextConfig
