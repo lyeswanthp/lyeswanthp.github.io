7626e918154cc24a43d8fa6f22bad667835ac530
lyeswanthp <lpanch2@emory.edu>
2025-05-11 00:02:56 -0400

    Initial portfolio website

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['github.com', 'raw.githubusercontent.com'],
+    unoptimized: true,
   },
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/{repository-name}' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/{repository-name}' : '',
 }
 
 module.exports = nextConfig
